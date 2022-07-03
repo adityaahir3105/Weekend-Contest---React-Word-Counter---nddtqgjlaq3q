fd939230d2d2bf0cfca6b052320c04c7a847c1a7
Aditya Ahir <65412259+adityaahir3105@users.noreply.github.com>
2022-07-03 14:26:31 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,64 +1,59 @@
-import React,{useState} from 'react'
-import '../styles/App.css';
+import React, { useState } from "react";
+import "../styles/App.css";
 const App = () => {
-  const [count,setCount] = useState(0);
-  const [charcount,setCharcount] =useState(0);
-  const [maxlen, setMaxlen]=useState(50);
-  const [size,setSize]=useState(16);
+  const [content, setContent] = useState(null);
+  const [fontSize, setFontSize] = useState(16);
+  const [limit, setLimit] = useState(50);
 
-  let change = (e) =>{
-    wordcount(e.target.value);
-  }
+  const getWords = () => {
+    return content == null || content == ""
+      ? 0
+      : content.trim().split(" ").length;
+  };
 
-  function wordcount(str){
-    let c=0;
-    let str1=str.split(" ");
-    charCout(str);
-
-    for(let i=0;i<str1.length;i++){
-      if(str1.length===0){
-        setCount(0);
-      }
-      if(str1[i]!==""){
-        c++;
-      }
-      setCount(c);
-    }
-  }
-
-  function charCout(str){
-    let c=0;
-    for(var i=0;i<str.length;i++){
-        c++;
-    setCharcount(c);
-    }
-    let maxNum = (e) => {
-      setMaxlen(e.target.value);
-    };
-
-    const sizeChanger = (e) => {
-      setSize(e.target.value);
-    }
-  }
+  const getChar = () => {
+    return content == null || content == "" ? 0 : content.split("").length;
+  };
 
   return (
     <div id="main">
-      <div >  
-        <label>Font Size Picker</label>
-        <input id='fontSize-input' defaultValue="16" type="range" min="16" max="32" onChange={sizeChanger}></input>
-        <label>Word limit input</label>
-        <input defaultValue="50" type="number" onChange={maxNum} id="char-limit-input"></input>
-        <br/>
-        <textarea
-        onChange={change}
-        maxLength={maxlen}
-        style={{fontSize:`${size}px`}}></textarea>
-        <div id="word-counter"> Total Number of words Written:{count}</div>
-        <div id="char-length">Total Number of Char count:{charcount}</div>
+      <div>
+        <input
+          type="range"
+          id="fontSize-input"
+          min="16"
+          max="32"
+          value={fontSize}
+          onChange={(e) => setFontSize(e.target.value)}
+        />
       </div>
+      <br />
+      <div>
+        <input
+          type="text"
+          id="char-limit-input"
+          value={limit}
+          onChange={(e) => setLimit(e.target.value)}
+        />
+      </div>
+      <br />
+      <div>
+        <textarea
+          onChange={(e) => {
+            setContent(e.target.value);
+          }}
+          style={{ fontSize: fontSize + "px" }}
+          maxLength={limit}
+        >
+          {content}
+        </textarea>
+      </div>
+      <br />
+      <div id="word-counter">{getWords()}</div>
+      <br />
+      <div id="char-counter">{getChar()}</div>
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
